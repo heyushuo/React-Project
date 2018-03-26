d13d125ea093e6e26d20ff53c56bf5cbcb430ec3
heyushuo <1007561607@qq.com>
2018-03-26 23:14:36 +0800

    json

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {
+	BrowserRouter,
+	Route,
+	Link
+} from 'react-router-dom'
 
 import { Provider } from 'react-redux'
-
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+
 	</Provider>,
 	document.getElementById('root'));
-registerS erviceWorker();
+
+
