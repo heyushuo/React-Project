1bb22841d4bbceae7c954eb05b7c8f6be65f991b
heyushuo <1007561607@qq.com>
2018-03-27 22:53:33 +0800

    first

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import {BrowserRouter, Route, Link} from 'react-router-dom'
 //Redux需要用到的
-import { createStore } from 'redux'
+import { createStore ,applyMiddleware ,compose } from 'redux'
 import { counter } from "./index.redux";
 import { Provider } from 'react-redux'
+
+
+import reducers from './reducer'
+import './config'
 //Redux需要用到的
 const store = createStore(
-	counter,
-	// 触发 redux-devtools
-	window.devToolsExtension ? window.devToolsExtension() : undefined
+	reducers,
+	compose(
+		applyMiddleware(thunk),
+		window.devToolsExtension ? window.devToolsExtension() : undefined
+	)
 )
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
 		</BrowserRouter>
-
 	</Provider>,
-	document.getElementById('root'));
+	document.getElementById('root')
+);
 
 
